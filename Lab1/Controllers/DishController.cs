28c5c4c3206b295188bec5ab426d1e3165da61a6
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-09-22 11:35:47 +0200

    Added ID property for Dish DTO

--- Lab1/Controllers/DishController.cs
+++ Lab1/Controllers/DishController.cs
@@ -18,7 +18,7 @@ namespace Lab1.Controllers
 		}
 
 		[HttpGet]
-		[Route("getalldishes")]
+		[Route("getAllDishes")]
 
 		public async Task<ActionResult<IEnumerable<Dish>>> GetAllDishes()
 		{
@@ -28,7 +28,7 @@ namespace Lab1.Controllers
 		}
 
 		[HttpGet]
-		[Route("getdishbyid/{dishId}")]
+		[Route("getDishById/{dishId}")]
 
 		public async Task<ActionResult<Dish>> GetDishById(int dishId)
 		{
@@ -38,7 +38,7 @@ namespace Lab1.Controllers
 		}
 
 		[HttpPost]
-		[Route("addnewdish")]
+		[Route("addNewDish")]
 		public async Task<ActionResult> AddDish(DishDTO dish)
 		{
 			await _dishServices.AddDishAsync(dish);
@@ -47,20 +47,22 @@ namespace Lab1.Controllers
 		}
 
 		[HttpDelete]
-		[Route("deletedishbyid")]
+		[Route("deleteDishById/{dishId}")]
 		public async Task<ActionResult> DeleteDish(int dishId)
 		{
 			await _dishServices.DeleteDishAsync(dishId);
 			return Ok();
 		}
 
-		[HttpPatch]
-		[Route("updatedish")]
+		[HttpPut]
+		[Route("updateDishById/{dishId}")]
 
-		public async Task<ActionResult> UpdateDish(int dishToUpdate, DishDTO newDish)
+		public async Task<ActionResult> UpdateDish(int dishId, DishDTO newDish)
 		{
-			await _dishServices.UpdateDishAsync(dishToUpdate, newDish);
-			return Ok();
+            Console.WriteLine(newDish.Availability);
+            await _dishServices.UpdateDishAsync(dishId, newDish);
+			
+            return Ok();
 		}
 	}
 }
